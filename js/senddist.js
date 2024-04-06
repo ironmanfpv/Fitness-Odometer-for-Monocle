420ea68ce4138b1a3366234fee8b1aa50971bd93
ironmanfpv <akitaishi@gmail.com>
2024-04-06 22:09:06 +0800

    Update senddist.js

--- js/senddist.js
+++ js/senddist.js
@@ -6,8 +6,8 @@ async function sendDistanceData(){
     if (myMonocle && myMonocle.data_send) {
         // Check if distanceBox.textContent is available in tracker.js
         if (typeof distanceBox.textContent !== 'undefined'){
-            console.log("Distance to be sent:", distanceBox.textContent);                   // Debugging statement
             myMonocle.repl('import display; display.clear()');
+            console.log("Distance to be sent:", distanceBox.textContent);                   // Debugging statement
             myMonocle.repl('import display; display.show(display.Text("' + distanceBox.textContent + '"))\x04');
         } else {
             console.log("distanceBox.textContent is undefined or empty.");                  // Debugging statement
