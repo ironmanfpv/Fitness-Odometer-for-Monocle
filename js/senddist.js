780a0dad1997167a5ad20362e72c57b144a72bbb
ironmanfpv <akitaishi@gmail.com>
2024-04-07 16:37:40 +0800

    Update senddist.js

--- js/senddist.js
+++ js/senddist.js
@@ -7,7 +7,7 @@ async function sendDistanceData(){
     //setInterval(sendDistanceData, 3000);                    // Line for repeat call to be within function
     //myMonocle.repl('import display; display.clear()');      // Line for clear display to be within function
 
-    myMonocle.repl('import display; display.show(display.Text("Total Distance Covered", 220, 100, display.WHITE, justify=display.MIDDLE_CENTER))\n');
+    //myMonocle.repl('import display; display.show(display.Text("Total Distance Covered", 220, 100, display.WHITE, justify=display.MIDDLE_CENTER))\n');
 
     try {
         if (myMonocle && myMonocle.data_send) {
@@ -16,8 +16,8 @@ async function sendDistanceData(){
                 Data = distanceBox.textContent
                 myMonocle.repl('import display; display.clear()\n');
                 myMonocle.repl('import led; led.on(led.GREEN)\n');                                        //Checks if code reach into this line
-                //myMonocle.repl('import display; display.show(display.Text("Total Distance Covered", 220, 100, display.WHITE, justify=display.MIDDLE_CENTER))\n');
-                myMonocle.repl('import display; display.show(display.Text("' + Data + 'KM", 320, 200, display.GREEN, justify=display.MIDDLE_CENTER))\n');
+                myMonocle.repl('import display; display.show(display.Text("Total Distance Covered", 220, 100, display.WHITE, justify=display.MIDDLE_CENTER))\n');
+                myMonocle.repl('import display; display.show(display.Text("' + Data + 'KM", 320, 200, display.WHITE, justify=display.MIDDLE_CENTER))\n');
                 myMonocle.repl('import utime; time.sleep(1.0)\n');
                 //myMonocle.repl('import display; line = display.Line(175, 230, 465, 230, display.WHITE)\x04');
                 //myMonocle.repl('import display; display.show(text,line)\x04');
