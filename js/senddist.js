1e0b70d59367a60ea4c6f749db8fc558c5d69c3c
ironmanfpv <akitaishi@gmail.com>
2024-04-07 16:20:39 +0800

    Update senddist.js

--- js/senddist.js
+++ js/senddist.js
@@ -1,6 +1,6 @@
 // Function to send distance data
 
-setInterval(sendDistanceData, 10000);
+setInterval(sendDistanceData, 2000);
 
 async function sendDistanceData(){
 
@@ -13,14 +13,13 @@ async function sendDistanceData(){
             if (typeof distanceBox.textContent !== 'undefined' && distanceBox.textContent.trim() !== ''){ // Check if distanceBox.textContent is available in tracker.js
                 Data = distanceBox.textContent
                 myMonocle.repl('import display; display.clear()\n');
-                myMonocle.repl('import led; led.on(led.GREEN)\n');    //Checks if code reach into this line
-                myMonocle.repl('import display; display.show(display.Text("' + Data + 'KM", 320, 200, display.WHITE, justify=display.MIDDLE_CENTER))\n');
-                myMonocle.repl('import utime; time.sleep(1.5)\n');
+                myMonocle.repl('import led; led.on(led.GREEN)\n');                                        //Checks if code reach into this line
+                myMonocle.repl('import display; display.show(display.Text("Total Distance Covered", 220, 100, display.WHITE, justify=display.MIDDLE_CENTER))\n');
+                myMonocle.repl('import display; display.show(display.Text("' + Data + 'KM", 320, 200, display.GREEN, justify=display.MIDDLE_CENTER))\n');
+                myMonocle.repl('import utime; time.sleep(1.0)\n');
                 //myMonocle.repl('import display; line = display.Line(175, 230, 465, 230, display.WHITE)\x04');
                 //myMonocle.repl('import display; display.show(text,line)\x04');
-                //myMonocle.repl('import device; device.battery_level()');
                 //myMonocle.repl('import device; import display; display.show(device.battery_level())');
-                //myMonocle.repl('import utime; time.sleep(0.5)');
                 //console.log("Distance to be sent:", distanceBox.textContent);                                             // Debugging statement
                 //myMonocle.repl('import display; display.show(display.Text("' + distanceBox.textContent + 'KM"))\x04');    // Print w/o format
             } else {
