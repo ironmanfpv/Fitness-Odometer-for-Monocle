6ad191f50edf8cc058306f23174b9dd8dec73f01
ironmanfpv <akitaishi@gmail.com>
2024-04-07 14:17:18 +0800

    Update senddist.js

--- js/senddist.js
+++ js/senddist.js
@@ -33,11 +33,11 @@ async function sendDistanceData(){
 // Function to send distance data
 
 //myMonocle.repl('import display; display.clear()\x04');
-setInterval(sendDistanceData, 3000);
+//setInterval(sendDistanceData, 3000);
 
 async function sendDistanceData(){
 
-    //setInterval(sendDistanceData, 3000);
+    setInterval(sendDistanceData, 3000);
     //myMonocle.repl('import display; display.clear()');
 
     try {
@@ -46,9 +46,9 @@ async function sendDistanceData(){
             if (typeof distanceBox.textContent !== 'undefined' && distanceBox.textContent.trim() !== ''){ // Check if distanceBox.textContent is available in tracker.js
                 myMonocle.repl('import display; display.clear()\x04');
                 myMonocle.repl('import led; led.on(led.GREEN)\x04');
-                //text = display.Text('distanceBox.textContent', 320, 200, display.WHITE, justify=display.MIDDLE_CENTER);
-                //line = display.Line(175, 230, 465, 230, display.WHITE);
-                //myMonocle.repl('import display; display.show(text,line)\x04');
+                myMonocle.repl('import display; text = display.Text("Hello", 320, 200, display.WHITE, justify=display.MIDDLE_CENTER)\x04');
+                myMonocle.repl('import display; line = display.Line(175, 230, 465, 230, display.WHITE)\x04');
+                myMonocle.repl('import display; display.show(text,line)\x04');
                 //myMonocle.repl('import device; device.battery_level()');
                 //myMonocle.repl('import device; import display; display.show(device.battery_level())');
                 //myMonocle.repl('import utime; time.sleep(0.5)');
