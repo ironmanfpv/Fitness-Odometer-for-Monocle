87730fe5461b70463ba3196d6d39c71e121cf7ce
ironmanfpv <akitaishi@gmail.com>
2024-04-07 17:33:46 +0800

    Update senddist.js

--- js/senddist.js
+++ js/senddist.js
@@ -17,10 +17,19 @@ async function sendDistanceData(){
                 myMonocle.repl('import display; display.clear()\n');                                      //clears the rainbow test
                 myMonocle.repl('import led; led.on(led.GREEN)\n');                                        //Checks if code reach into this line
                 
-                myMonocle.repl('display.show(\n');
-                myMonocle.repl('display.Text("Total Distance Covered", 220, 100, display.WHITE, justify=display.TOP_LEFT),\n');
-                myMonocle.repl('display.Text("' = + Data + 'KM", 320, 200, display.WHITE, justify=display.MIDDLE_CENTER)\n');
-                myMonocle.repl(')\n');
+                myMonocle.repl(`
+                              import display;
+                              t1 = display.Text("Total Distance Covered", 20, 100, display.WHITE, display.TOP_LEFT);
+                              t2 = display.Text('${Data} KM', 320, 200, display.WHITE, display.MIDDLE_CENTER);
+                              display.show(t1, t2);
+                          `);
+
+
+
+                // myMonocle.repl('display.show(\n');
+                //myMonocle.repl('display.Text("Total Distance Covered", 220, 100, display.WHITE, justify=display.TOP_LEFT),\n');
+                //myMonocle.repl('display.Text("' = + Data + 'KM", 320, 200, display.WHITE, justify=display.MIDDLE_CENTER)\n');
+                //myMonocle.repl(')\n');
                 
                 
                 
